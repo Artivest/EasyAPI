1889548481e022970e82be7479dc4f158d20e86f
Adrian Czebiniak <adrian@artivest.co>
2014-10-17 17:50:14 -0400

    Updated txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='EasyAPI',
-    version='0.1.2',
+    version='0.1.3',
     author='Adrian Czebiniak',
     author_email='adrian@artivest.co',
     packages=['easy_api', 'easy_api.test'],
